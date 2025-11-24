ca8ed85b20eed60662a2b0f34f2d7969e5e7a15e
supchyan <mikhailsupov@yandex.ru>
2025-11-24 17:55:26 +0300

    wawa, more docs

--- delib.js
+++ delib.js
@@ -1,3 +1,6 @@
+/**
+ * Class to handle a delib device instance.
+ */
 class DelibDevice {
     constructor(ip, secret = null) {
         this.ip = ip;
@@ -5,7 +8,7 @@ class DelibDevice {
     }
     /**
      * 
-     * @param {*} cmd command to send to the de:things device.
+     * @param {*} cmd string command to send to the delib device.
      * @returns Device response as `text/plain` string.
      */
     async call(cmd) {
@@ -20,17 +23,17 @@ class DelibDevice {
             // http://1.2.3.4:80/anything_else <- `anything_else` handler path
             await fetch(`http://${this.ip}:80/${cmd}`, { method: "POST", body: this.secret })
             .then(res => res.text()).then(data => {
-                response = data;
+                response = data; // save a wlan handler response
             });
         }
         else { 
             // send req to the ethernet handler
-            // delib devices accept ethernet commands like:
+            // delib devices handle ethernet requests like commands with '!' in the beggining:
             // !on, !off, !anything_else
-            // so sender specifies, what stands after '!' char.
+            // so sender specifies, which "command" stands after '!' char.
             await fetch(`http://${this.ip}:80/`, { method: "POST", body: `!${cmd}` })
             .then(res => res.text()).then(data => {
-                response = data;
+                response = data; // save ethernet response
             });
         }
 
